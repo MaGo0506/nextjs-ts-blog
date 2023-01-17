8d9232f8e958786d0a01b47a63a015471a132c84
Marko Goronjic <marko.goronjic@quantox.com>
2023-01-17 19:06:55 +0100

    Implemented Preview mode, created Blog Component, setting up Type definitions

---
+++ components/BlogList.tsx
@@ -0,0 +1,12 @@
+import {Post} from "../typings";
+
+type Props = {
+    posts: Post[];
+}
+const BlogList = ({ posts }: Props) => {
+    return (
+        <div></div>
+    );
+}
+
+export default BlogList
