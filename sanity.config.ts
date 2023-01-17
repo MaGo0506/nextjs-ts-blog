712b4d0240c8b6d2e25164e926f8e913e39a162a
Marko Goronjic <marko.goronjic@quantox.com>
2023-01-17 22:42:44 +0100

    Upgrading to preview mode

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,28 +5,33 @@ import {schemaTypes} from './schemas'
 import {myTheme} from "./theme";
 import studioNavbar from "./components/StudioNavbar";
 import Logo from './components/Logo';
+import {getDefaultDocumentNode} from "./structure";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineConfig({
-  basePath: '/studio',
-  name: 'ZVELTE_Blog_Content',
-  title: 'ZVELTE Blog Content',
+    basePath: '/studio',
+    name: 'ZVELTE_Blog_Content',
+    title: 'ZVELTE Blog Content',
 
-  projectId,
-  dataset,
+    projectId,
+    dataset,
 
-  plugins: [deskTool(), visionTool()],
-
-  schema: {
-    types: schemaTypes,
-  },
-  studio: {
-    components: {
-      logo: Logo,
-      navbar: studioNavbar,
-    }
-  },
-  theme: myTheme,
+    plugins: [
+        deskTool({
+            defaultDocumentNode: getDefaultDocumentNode,
+        }),
+        visionTool()
+    ],
+    schema: {
+        types: schemaTypes,
+    },
+    studio: {
+        components: {
+            logo: Logo,
+            navbar: studioNavbar,
+        }
+    },
+    theme: myTheme,
 })
